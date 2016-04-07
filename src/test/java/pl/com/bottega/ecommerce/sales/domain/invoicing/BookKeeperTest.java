2eb17b5f02fee86ab8bda736013b6499007e7925
HRN7 <hrn@onet.pl>
2016-04-07 19:00:09 +0200

    save work

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,32 +5,47 @@
  */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Matchers.any;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 /**
  *
  * @author student
  */
 public class BookKeeperTest {
-    
+    InvoiceFactory invoiceFactory;
+    TaxPolicy tax;
     @Before
     public void setForTest(){
-        InvoiceFactory invoiceFactory = Mockito.mock(InvoiceFactory.class);
-        InvoiceRequest invoiceRequest = Mockito.mock(InvoiceRequest.class);
+        invoiceFactory = mock(InvoiceFactory.class);
+        tax = mock(TaxPolicy.class);
         
-        ClientData client = new ClientData(Id.generate(), "client");
-        when(invoiceFactory.create(client)).thenReturn(new Invoice(Id.generate(), client));
+        
+        
+       
+        
     }
     
     @Test
     public void givenInvoiceRequestWithSingleItem_whenIssuance_thenInvoiceWithSingleItem(){
-
+        ClientData client = new ClientData(Id.generate(), "name");
+        Invoice invoice = new Invoice(Id.generate(), client);
+        InvoiceRequest invoiceReq = new InvoiceRequest(client);
+        ProductData product = new ProductData();
+        RequestItem requestItem = new requestItem();
+        when(invoiceFactory.create(client)).thenReturn(invoice);
+        when(tax.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, null));
         
     }
     
