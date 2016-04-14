22ee12ff5a6f168b1e098bf7644bc10af7b51f4e
MichalakJ <daran.dasaner@gmail.com>
2016-04-14 19:22:11 +0200

    save changes

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommanderHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommanderHandlerTest.java
@@ -5,9 +5,17 @@
  */
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
+import java.util.Date;
 import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 
 /**
  *
@@ -15,10 +23,27 @@ import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
  */
 public class AddProductCommanderHandlerTest {
     
-    AddProductCommand command;
+    private AddProductCommand command;
+    private AddProductCommandHandler handler;
+    private ReservationRepository reservationRepository;
     @Before
     public void setUpForTest(){
         command = new AddProductCommand(Id.generate(), Id.generate(), 2);
+        handler = new AddProductCommandHandler();
+        Reservation reservation = new Reservation(Id.generate(), Reservation.ReservationStatus.CLOSED,
+                new ClientData(Id.generate(), "client"), new Date());
+        
+        reservationRepository = mock(ReservationRepository.class);
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+        
+        handler.setReservationRepository(reservationRepository);
+        
+        
+    }
+    
+    
+    @Test
+    public void someTest(){
         
     }
     
